af0962ab2b58cd568924aa0ccac6f5435830416b
Ayb001 <aghjedimayoub@gmail.com>
2025-09-02 15:15:45 +0100

    cet exercice est simple, il suffit de convertir la température et d'explorer la variable float

--- Variables/Challenge2/Challenge2.c
+++ Variables/Challenge2/Challenge2.c
@@ -4,6 +4,7 @@ int main()
     float Celsius;
     float Kelvin;
     printf("entrer la température en Celsius:");
+
     scanf("%f",&Celsius);
     Kelvin = Celsius + 273.15;
     printf("%f",Kelvin);
