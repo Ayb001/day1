4ce259c618e19edce7e0bffa7355f667bfb51849
Ayb001 <aghjedimayoub@gmail.com>
2025-09-02 22:04:22 +0100

    Calcul de la surface d'un rectangle à partir de la longueur et largeur

---
+++ Variables/Challenge11/Challenge11.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int la,lo,s;
+    printf("entrer longeur:");
+    scanf("%d",&la);
+    printf("entrer largeur:");
+    scanf("%d",&lo);
+    s=la*lo;
+    printf("la surface de rectangle est %d\n",s);
+}
