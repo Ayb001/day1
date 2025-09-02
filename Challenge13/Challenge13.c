5881743d20e73ed9a826f6206f73fb80490dcf61
Ayb001 <aghjedimayoub@gmail.com>
2025-09-02 14:31:15 +0100

    Cet exercice consiste à convertir des nombres décimaux en binaires et hexadécimaux. Pour la conversion binaire, c'était facile, et pour l'hexadécimal, je rencontrais un problème, j'avais de mauvaises réponses, hhhh et j'ai découvert que je divisais par 10 et non par 16, c'est pourquoi il me donnait une mauvaise réponse.

---
+++ Challenge13/Challenge13.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+int main()
+{
+    char str1[100];
+    char str2[100];
+    char str3[]="0123456789ABCDEF";
+    int x;
+    int i=0;
+    int j=0;
+    int temp;
+    printf("entrer nombre:");
+    scanf("%d",&x);
+    temp=x;
+    while(x>0)
+    {
+        str1[i]=x%2+'0';
+        x=x/2;
+        i++;
+    }
+    str1[i]='\0';
+    while(temp>0)
+    {
+        str2[j]=str3[temp%16];
+        temp=temp/16;
+        j++;
+    }
+    str2[j]='\0';
+    i--;
+    j--;
+    printf("binare\n");
+    while(i>=0)
+    {
+        printf("%c",str1[i]);
+        i--;
+    }
+    printf("\n");
+    printf("hexa decimal\n");
+    while(j>=0)
+    {
+        printf("%c",str2[j]);
+        j--;
+    }
+}
