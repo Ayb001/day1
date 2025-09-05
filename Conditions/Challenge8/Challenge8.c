abf2374caf9b5454fe47ba4b731283a97e9f9e8f
Ayb001 <aghjedimayoub@gmail.com>
2025-09-05 21:12:54 +0100

    afficher la mention obtenue par un élève en fonction de la moyenne de ses notes.

---
+++ Conditions/Challenge8/Challenge8.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+   int a;
+   printf("entrer votre moyenne :");
+   scanf("%d",&a);
+   if(a>=0  && a<=20){
+       if(a<10)
+       {
+       printf("recalé");
+   }
+       else if(a>=10 && a<12)
+       {
+           printf("passable");
+   }
+       else if(a>=12 && a<14)
+       {
+           printf("assez bien");
+       }
+       else if(a>=14 && a<16)
+       {
+           printf("bien");
+       }
+       else
+       {
+       printf("Tree bien");
+       }
+   }
+   else
+   {
+       printf("entrer une moyenne entre 0 et 20");
+   }
+    return 0;
+}
+
