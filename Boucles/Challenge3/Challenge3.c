f35fc094ed691374bb3e064e0cbc46ca050d6120
Ayb001 <aghjedimayoub@gmail.com>
2025-09-07 18:14:16 +0100

    Somme des N Nombres

---
+++ Boucles/Challenge3/Challenge3.c
@@ -0,0 +1,8 @@
+int main() {
+int n, i, sum = 0;
+printf("Entrer un nombre : ");
+scanf("%d", &n);
+for (i = 1; i <= n; i++) sum += i;
+printf("Somme des %d premiers nombres = %d\n", n, sum);
+return 0;
+}
